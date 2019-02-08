8cf9a4f0fd21ab8f55d7aa0806eeadff2185d893
Gabriel Santos <aec.gabrielsantos@gmail.com>
2019-02-08 14:29:24 -0300

    Changed camera.py to be just a camera launch
    The camera.py file is a library that launches the camera to be used on other applications

--- assimilation/src/Scripts/camera.py
+++ assimilation/src/Scripts/camera.py
@@ -16,22 +16,29 @@ class camera(object):
         self.bridge_object = CvBridge()
         self.image_sub = rp.Subscriber("/rexrov/rexrov/camera/camera_image", Image, self.camera_callback)
         self.close = False
+        self.ready = False
 
 
     def camera_callback(self,data):
         try:
-            cv_image = self.bridge_object.imgmsg_to_cv2(data, desired_encoding="bgr8")
-            cv2.imshow("Rexrov camera", cv_image)
-            cv2.waitKey(1)
+            self.cv_image = self.bridge_object.imgmsg_to_cv2(data, desired_encoding="bgr8")
+            self.ready = True
 
         except CvBridgeError as e:
             self.close = True
             rp.logfatal(e)
     
+    def get(self):
+        if (self.ready):
+            return self.cv_image
+        else:
+            empty_img = np.zeros((492,768,3),np.uint8)
+            return empty_img
 
+    def is_ready(self):
+        return self.ready
 def main():
 
-    rexrov_camera = camera()
     rp.init_node("camera_node",anonymous=True)
     while(not rp.is_shutdown()):
         rp.spin()
--- assimilation/src/Scripts/color_ident.py
+++ assimilation/src/Scripts/color_ident.py
@@ -7,6 +7,26 @@ import rospy as rp
 from cv_bridge import CvBridge, CvBridgeError
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Image
+import camera
+
 
 class color_ident():
+    def __init__(self):
+        self.run = camera.camera()
+
+    def convert_HSV(self):
+        img = self.run.get()
+        cv2.imshow("Rexrov camera", img)
+        cv2.waitKey(3)
+        
+def main():
     
+    rp.init_node("camera_node",anonymous=True)
+    run = color_ident()
+    while(not rp.is_shutdown()):
+        if(run.run.is_ready()):
+            run.convert_HSV()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
