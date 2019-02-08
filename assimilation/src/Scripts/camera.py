bb1c5bc2e70d2d560451e956baa2cc07203db0e8
Gabriel Santos <aec.gabrielsantos@gmail.com>
2019-02-08 16:24:47 -0300

    HSV conversion added

--- assimilation/src/Scripts/camera.py
+++ assimilation/src/Scripts/camera.py
@@ -8,20 +8,19 @@ from cv_bridge import CvBridge, CvBridgeError
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Image
 
-class camera(object):
+class launch(object):
 
     def __init__(self):
-        
         
         self.bridge_object = CvBridge()
         self.image_sub = rp.Subscriber("/rexrov/rexrov/camera/camera_image", Image, self.camera_callback)
         self.close = False
         self.ready = False
 
-
     def camera_callback(self,data):
         try:
             self.cv_image = self.bridge_object.imgmsg_to_cv2(data, desired_encoding="bgr8")
+            self.height, self.width, self.channels = self.cv_image.shape
             self.ready = True
 
         except CvBridgeError as e:
@@ -32,17 +31,17 @@ class camera(object):
         if (self.ready):
             return self.cv_image
         else:
-            empty_img = np.zeros((492,768,3),np.uint8)
+            empty_img = np.zeros((self.height,self.width,3),np.uint8)
             return empty_img
 
     def is_ready(self):
         return self.ready
+
 def main():
 
     rp.init_node("camera_node",anonymous=True)
     while(not rp.is_shutdown()):
         rp.spin()
-
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
--- assimilation/src/Scripts/color_ident.py
+++ assimilation/src/Scripts/color_ident.py
@@ -11,11 +11,20 @@ import camera
 
 
 class color_ident():
-    def __init__(self):
-        self.run = camera.camera()
 
-    def convert_HSV(self):
-        img = self.run.get()
+    def __init__(self):
+        self.start = camera.launch()
+        self.descentre = 160
+        self.rows_to_watch = 20
+        
+
+    def convert_to_HSV(self):
+        
+        hsv = cv2.cvtColor(self.start.cv_image, cv2.COLOR_BGR2HSV)
+        print(hsv)
+
+    def show_img(self):
+        img = self.start.get()
         cv2.imshow("Rexrov camera", img)
         cv2.waitKey(3)
         
@@ -24,8 +33,9 @@ def main():
     rp.init_node("camera_node",anonymous=True)
     run = color_ident()
     while(not rp.is_shutdown()):
-        if(run.run.is_ready()):
-            run.convert_HSV()
+        if(run.start.is_ready()):
+            run.show_img()
+            run.convert_to_HSV()
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
