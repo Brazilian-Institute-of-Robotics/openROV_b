ad9eafcad9bf5a1d17ab083efee1d0098e2e977c
Gabriel Santos <aec.gabrielsantos@gmail.com>
2019-02-15 13:20:56 -0300

    Redo joystick

--- assimilation/src/Scripts/joystick.py
+++ assimilation/src/Scripts/joystick.py
@@ -4,26 +4,27 @@ import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
 
-class Joystick():
-
-    def __init__(self):
-
-        self.joy_sub = rospy.Subscriber("joy", Joy, self.joy_callback)
-        self.cmd_pub = rospy.Publisher("cmd_vel", Twist, queue_size=10)
-
-        self._axes = dict(x=4, y=3, z=1,
-                          roll=2, pitch=5, yaw=0,
-                          xfast=-1, yfast=-1, zfast=-1,
-                          rollfast=-1, pitchfast=-1, yawfast=-1)
         
-        self._axes_gain = dict(x=3, y=3, z=0.5,
-                               roll=0.5, pitch=0.5, yaw=0.5,
-                               xfast=6, yfast=6, zfast=1,
-                               rollfast=2, pitchfast=2, yawfast=2)
-        
-    def joy_callback(self, msg):
+def joy_callback(data):
 
-        self._axes["x"]
+    twist = Twist()
+    twist.linear.x = 4*data.axes[7]
+    twist.angular.z = 4*data.axes[6]
+    cmd_pub.publish(twist)
+
+def start():
+
+    global cmd_pub
+
+    joy_sub = rospy.Subscriber("joy", Joy, joy_callback)
+    cmd_pub = rospy.Publisher("cmd_vel", Twist, queue_size=10)
+    rospy.init_node('Joy_controller')
+    rospy.spin()
+        
+
+
+
+        
 
 
 
