9274ddefa62c0c67d2e400dacc810c5ef6fbd1f0
Gabriel Santos <aec.gabrielsantos@gmail.com>
2019-02-19 15:43:04 -0300

    Go to goal files to move the robot with the camera on

---
+++ assimilation/src/Go to goal/Scripts/joystick.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import Twist, Vector3
+from sensor_msgs.msg import Joy
+
+class Joystick_control():
+
+    def __init__(self):
+        
+        self.joy_sub = rospy.Subscriber("joy", Joy, self.joy_callback)
+        self.cmd_pub = rospy.Publisher("cmd_vel", Twist, queue_size=10)
+        self.linear = Vector3(0,0,0)
+        self.angular = Vector3(0,0,0)
+        self.deadman = 0
+    
+    def joy_callback(self,data):
+
+        self.angular.z = data.axes[0]
+        self.linear.x = data.axes[1]
+        self.linear.y = data.axes[2]
+        self.linear.z = data.axes[3]
+        self.deadman = data.buttons[5]
+
+    def control(self):
+        
+        ctrl = Twist()
+
+        if self.deadman != -1:
+            ctrl.linear = self.linear
+            ctrl.angular = self.angular
+
+        else:
+            ctrl.linear = Vector3(0,0,0)
+            ctrl.angular = Vector3(0,0,0)
+        
+        self.cmd_pub.publish(ctrl)
+    
+def start():
+    run= Joystick_control()
+
+    rospy.init_node('Joy_controller', anonymous=True)
+    
+    while(not rospy.is_shutdown()):
+        run.control()
+
+
+        
+
+if __name__ == "__main__":
+    start()    
+
+
+
+        
+
+
+
+        
+        
+
